0377d39ec3f7f60832df35ac1b4881c5ed7fb431
bhagwat dixit <Bhagwatdixit55@gmail.com>
2024-02-28 09:23:06 +0530

    dom

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = document.getElementById("container");
+
+
+a.style.alignItems = "centre"
+a.style.backgroundColor = "yellow"
+a.style.color = "blue"
+a.style.margin = "20px"
+a.style.padding = "10px"
+a.style.fontSize = "18px"
+a.style.fontWeight = "bold"
+a.style.height = "200px"
+a.style.width = "300px"
